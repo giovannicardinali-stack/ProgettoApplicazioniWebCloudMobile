f4fdf2b5f174964701c4d4d5b96257020ae69d3a
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-06-15 23:30:18 +0200

    creata cartella frontend

---
+++ frontend/src/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+//configurazione indirizzo
+const api = axios.create({
+    URL: 'http://localhost:8080'
+});
+
+//codice per passare automaticamente il token dal browser all'header
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('jwtToken');
+    if(token){
+        config.headers.Authorization = 'Bearer ${token}';
+    }
+    return config;
+
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default api;
