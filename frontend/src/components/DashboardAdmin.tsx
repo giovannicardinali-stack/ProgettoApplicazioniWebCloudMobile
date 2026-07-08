32c4503f6cb80a78c6bd8a0eb927a5d8a6c0134c
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-07-08 22:46:30 +0200

    inizio implementazione dashboard dell'admin

---
+++ frontend/src/components/DashboardAdmin.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+const DashboardAdmin = () => {
+    const[vistaCorrente, setVistaCorrente] = useState("HOME");
+    
+}
+
+return(
+    <div className="d-flex">
+        
+    </div>
+)
